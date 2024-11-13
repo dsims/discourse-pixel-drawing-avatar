939ea71004814e46f1820b7dc26469c00478f2c3
dsims <dsims@users.noreply.github.com>
2024-11-13 01:31:26 -0500

    Fix due to uppy mixin refactor
    3.4.0.beta3-dev (ad39688)

--- javascripts/discourse/initializers/pixel-drawing-avatar-uploader.js
+++ javascripts/discourse/initializers/pixel-drawing-avatar-uploader.js
@@ -15,7 +15,7 @@ export default {
           const file = new File([buf], "avatar-drawing.png", {
             type: "image/png",
           });
-          await this._addFiles(file);
+          await this.uppyUpload.addFiles(file);
         },
       });
     });
